b34d358ea5f99c7a655cdb2bcc2d1048f0ff124b
Madhura Mendis <madhuramendis@gmail.com>
2025-12-04 12:09:58 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://madhuramendis.github.io',
+  url: '/openchoreo-website-staging/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
