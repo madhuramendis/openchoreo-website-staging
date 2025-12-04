17ecf41b6ed623e7326ebaeef1431a0b60927f5a
Madhura Mendis <madhuramendis@gmail.com>
2025-12-04 12:03:09 +0530

    Update site URL for staging deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://openchoreo.dev',
+  url: 'https://madhuramendis.github.io/openchoreo-website-staging/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
